d22b50e01620081512a54678d3395c780d1a3bbb
Michael Bastos <bastosmichael@gmail.com>
2023-07-31 17:46:21 -0500

    removing tests

--- tests/test_models.py
+++
@@ -1,9 +0,0 @@
-from perplexity.models.chat_request import ChatRequest
-
-
-def test_chat_request_model():
-    data = {"conversation_id": "123", "message": "Hello, world!"}
-    chat_request = ChatRequest(**data)
-
-    assert chat_request.conversation_id == "123"
-    assert chat_request.message == "Hello, world!"
--- tests/test_templates.py
+++
@@ -1,5 +0,0 @@
-from perplexity.templates import CHAT_PROMPT_TEMPLATE
-
-
-def test_chat_prompt_template():
-    assert isinstance(CHAT_PROMPT_TEMPLATE.messages, list)
